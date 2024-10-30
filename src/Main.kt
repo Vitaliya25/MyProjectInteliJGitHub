00134a08e51331b142258311b5422b49b3f26adb
Vitaliya <vitaliyaskral@gmail.com>
2024-10-30 12:25:20 +0100

    Creation and using fun helloWorld

--- src/Main.kt
+++ src/Main.kt
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main(args: Array<String>) {
     println("Proyecto para probar IntelIJ con GitHub");
-    helloworld("Vitaliya")
+    helloWorld("Vitaliya")
 }
 fun  helloWorld(name: String){
     println("Hello $name")
